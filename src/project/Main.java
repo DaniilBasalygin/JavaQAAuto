cd0436f056c11ab62538087ae218ca75c5d639ef
Daniil Basalygin <basalygin.daniil@gmail.com>
2019-10-19 10:43:16 +0300

    2

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,9 +3,9 @@ package project;
 public class Main {
     public static void main(String[] args){
         System.out.println("hi");
-        System.out.println("ho");
-        System.out.println("ho");
-        System.out.println("ho");
+        System.out.println("hi");
+        System.out.println("hi");
+
 
 
     }
