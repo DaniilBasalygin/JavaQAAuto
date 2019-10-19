f7b57b54dee5191f9e5d62616cf7bd6b25bf7baf
Daniil Basalygin <basalygin.daniil@gmail.com>
2019-10-19 09:27:21 +0300

    commit3

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args){
         System.out.println("hi");
         System.out.println("ho");
+        System.out.println("ho");
+        System.out.println("ho");
+        System.out.println("ho");
 
     }
 }
