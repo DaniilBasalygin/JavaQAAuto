1a2732e58b608b4cda6f8c9b71b33cc1c7a38ddb
Daniil Basalygin <basalygin.daniil@gmail.com>
2019-10-19 10:56:06 +0300

    23

--- src/project/Main.java
+++ src/project/Main.java
@@ -7,6 +7,13 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
+        System.out.println("hi");
 
 
 
