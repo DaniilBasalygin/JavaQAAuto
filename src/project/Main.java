7b39776f1fb470649e71522855717d79af47a0d8
Daniil Basalygin <basalygin.daniil@gmail.com>
2019-10-19 10:37:09 +0300

    Testcommit5

--- src/project/Main.java
+++ src/project/Main.java
@@ -3,12 +3,7 @@ package project;
 public class Main {
     public static void main(String[] args){
         System.out.println("hi");
-        System.out.println("ho");
-        System.out.println("ho");
-        System.out.println("ho");
-        System.out.println("ho");
-        System.out.println("ho");
-        System.out.println("ho");
+
 
 
     }
